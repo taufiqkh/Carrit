3560cddc5135f13e84e3dc552d1c39c9b39d6615
Taufiq Hoven <taufiq.hoven@gmail.com>
2011-09-04 18:34:53 +1000

    Added basic verification of save directory.

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject carrit "0.1"
   :description "Minecraft Chunk Server"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]]
   :main carrit)
--- src/carrit.clj
+++ src/carrit.clj
@@ -1,5 +1,27 @@
 (ns carrit
+  (:require [clojure.contrib.logging :as logging]
+            [clojure.java.io :as io])
   (:gen-class))
+; Not idiomatic clojure; that will have to wait until I learn how to do things properly.
+  
+(def expected-save-entries #{"data" "region" "level.dat"})
+
+; Minecraft save directory, hard-coded to a test directory until I get around
+; to adding options
+(def minecraft-dir "Whole New World")
+
+(defn info
+  "Logs the specified string using a standard logger"
+  [log-message parameters]
+  (logging/info (format log-message parameters)))
+
+(defn verify-directory [directory]
+  (info "Verifying directory %s" (.getPath directory))
+  (if (.isDirectory directory)
+    (if (clojure.set/subset? expected-save-entries (into #{} (.list directory)))
+      directory
+      nil)
+    nil))
 
 (defn -main [& options]
-  (prn "Hello world"))
+  (verify-directory (io/file minecraft-dir)))
