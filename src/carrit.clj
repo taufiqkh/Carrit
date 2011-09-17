1ef012331492aae4c9445345ce4232c07a52381a
Taufiq Hoven <taufiq.hoven@gmail.com>
2011-09-18 09:55:12 +1000

    Added type hints and some chunk constants, file offset calculations.

--- src/carrit.clj
+++ src/carrit.clj
@@ -1,8 +1,9 @@
 (ns carrit
   (:require [clojure.contrib.logging :as logging]
-            [clojure.java.io :as io]
             [clojure.contrib.string :as string])
+  (:import [java.io File])
   (:gen-class))
+(set! *warn-on-reflection* true)
 ; Not idiomatic clojure; that will have to wait until I learn how to do things properly.
   
 (def EXPECTED_SAVE_ENTRIES #{"data" "region" "level.dat"})
@@ -10,36 +11,6 @@
 ; Minecraft save directory, hard-coded to a test directory until I get around
 ; to adding options
 (def MINECRAFT_DIR "Whole New World")
-
-(def COORDINATE_BIT_SIZE 32)
-
-(defn info
-  "Logs the specified string using a standard logger"
-  [log-message parameters]
-  (logging/info (format log-message parameters)))
-
-(defn verify-save-dir [directory]
-  "Verifies that a given directory contains the files/directories expected in a
-save game directory and returns a sequence of those files."
-  (info "Verifying directory %s" (.getPath directory))
-  (if (.isDirectory directory)
-    (let [files (file-seq directory) filenames (set (map #(.getName %) files))]
-      (if (clojure.set/subset? EXPECTED_SAVE_ENTRIES filenames)
-        files
-        nil))
-    nil))
-
-(defn extract-region-dir [files]
-  "Given a save game directory finds the region directory and extracts the
-files for that directory."
-  (if (empty? files)
-    nil
-    (let [first-file (first files)]
-      (if (= "region" first-file)
-        (if (.isDirectory first-file)
-          first-file
-          nil)
-        (recur (next files))))))
 
 (defn base-n
   "Converts a number to the specified base, using a digit sequence. The digit
@@ -59,13 +30,52 @@ so far."
         (recur base
                digits
                (/ (- number remainder) base)
-               (cons (.substring digits remainder (inc remainder)) acc))))))
+               (cons (string/get digits remainder) acc))))))
 
 ; Hackety hack
 (defn base-36 [number]
   "Converts a number to a string in base 36."
   (base-n number "0123456789abcdefjhijklmnopqrstuvwxyz"))
 
+(defn info
+  "Logs the specified string using a standard logger"
+  [log-message parameters]
+  (logging/info (format log-message parameters)))
+
+(defn verify-save-dir [^File directory]
+  "Verifies that a given directory contains the files/directories expected in a
+save game directory and returns a sequence of those files."
+  (info "Verifying directory %s" (.getPath directory))
+  (if (.isDirectory directory)
+    (let [files (file-seq directory) filenames (set (map (fn [^File file] (.getName file)) files))]
+      (if (clojure.set/subset? EXPECTED_SAVE_ENTRIES filenames)
+        files
+        nil))
+    nil))
+
+(defn extract-region-dir [files]
+  "Given a save game directory finds the region directory and extracts the
+files for that directory."
+  (if (empty? files)
+    nil
+    (let [^File first-file (first files)]
+      (if (= "region" first-file)
+        (if (.isDirectory first-file)
+          first-file
+          nil)
+        (recur (next files))))))
+
+(def ^{:doc "Size of a chunk, in blocks"} CHUNK_SIZE 32)
+(def ^{:doc "Size of chunk location data, in bytes" } CHUNK_LOCATION_SIZE 4)
+(def ^{:doc "Size of chunk timestamp data, in bytes"} CHUNK_TIMESTAMP_SIZE 4)
+(def ^{:doc "Size of the length header for chunk data, in bytes"} CHUNK_DATA_LENGTH_HEADER_SIZE 4)
+
+(defn calc-chunk-location-offset [x z]
+  "Given the x, y, z coordinates, calculates the offset, in bytes, of the
+chunk location in a region file"
+  (* CHUNK_LOCATION_SIZE (+ (mod x CHUNK_SIZE) (* (mod z CHUNK_SIZE) CHUNK_SIZE))))
+(defn chunk-location [location-byte-array]
+  nil)
 
 (defn read-chunk-file [file-name]
   (slurp file-name))
@@ -73,11 +83,11 @@ so far."
 (defn create-file-name [x y z]
   "Generates a region file name for the specified coordinates."
   (let [[xChunk yChunk zChunk]
-        (map #(with-precision 1 :rounding FLOOR (/ %1 COORDINATE_BIT_SIZE)) [x y z])]
+        (map #(with-precision 1 :rounding FLOOR (/ %1 CHUNK_SIZE)) [x y z])]
     format "r.%d.%d.mcr" [xChunk zChunk]))
 
 (defn -main [& options]
-  (if-let [save-dir-files (verify-save-dir (io/file MINECRAFT_DIR))]
+  (if-let [save-dir-files (verify-save-dir (File. MINECRAFT_DIR))]
     (if-let [region-dir-files (extract-region-dir save-dir-files)]
       nil
       nil)
